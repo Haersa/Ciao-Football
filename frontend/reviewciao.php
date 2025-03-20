66cd8a0744b9f2aa6d7222427cc0b9f06fb304b4
Harris Hamilton - Haersa <159532022+Haersa@users.noreply.github.com>
2025-03-20 15:55:31 +0000

    Added new components
    Added new components

--- frontend/reviewciao.php
+++ frontend/reviewciao.php
@@ -10,10 +10,41 @@ include('../components/header.php');
 
 <!-- Main content of the page starts here -->
 <main>
-  <section class="hero">
-    <h1>Leave a review </h1>
-  </section>
+<div class = "review-form-container"> <!-- start of review form container -->
+    <div class = "review-form-header"> <!-- review form heading -->
+        <h2>Review Ciao Football</h2>
+    </div><!-- end of review form heading -->
+    <div class="review-form-wrapper"> <!-- start of review form wrapper -->
+<form action="../backend/reviewciao.php" method="POST">
+  <div class="review-name-row">
+    <div class="review-name-field">
+      <label for="name">First Name:</label>
+      <input type="text" id="name" name="name" required>
+    </div>
+    
+    <div class="review-name-field">
+      <label for="surname">Last Name:</label>
+      <input type="text" id="surname" name="surname" required>
+    </div>
+  </div>
   
+  <div class="review-form-group">
+    <label for="rating">Rating (1.0 - 5.0):</label>
+    <input type="number" id="rating" name="rating" min="1" max="5" step="0.1" required>
+  </div>
+  
+  <div class="review-form-group">
+    <label for="review">Review:</label>
+    <textarea id="review" name="review" rows="4" required></textarea>
+  </div>
+  
+  <div class="review-form-group">
+    <button type="submit">Submit Review</button>
+  </div>
+</form>
+</div><!-- end of review form wrapper -->
+</div><!-- end of review form container -->
+
 </main>
 
 <?php
