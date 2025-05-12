7826600e548866874b0fb9d51ae2255b04d3cfbc
Harris <ec2060050@edinburghcollege.ac.uk>
2025-05-12 13:13:01 +0100

    small change to update error messages for no shirts found.

--- frontend/replicakits.php
+++ frontend/replicakits.php
@@ -99,7 +99,7 @@ if (isset($_GET['sort'])) {
         
         echo '</div>'; // End products container
     } else {
-        echo '<div class="no-products">No non-sale retro shirts found</div>'; // if no products are found, display this error to user
+        echo '<div class="no-products">No shirts found</div>'; // if no products are found, display this error to user
     }
 
     // Close statement
--- frontend/team.php
+++ frontend/team.php
@@ -110,7 +110,7 @@ include('../backend/conn/conn.php');
         
         echo '</div>'; // End products container
     } else {
-        echo '<div class="no-products">No non-sale retro shirts found</div>'; // if no products are found, display this error to user
+        echo '<div class="no-products">No shirts found</div>'; // if no products are found, display this error to user
     }
 
     // Close statement
